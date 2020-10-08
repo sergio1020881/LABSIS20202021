1355b23fce1447959c59cd965c4dcf4dc97f4f5c
sergio1020881 <sergio.salazar.santos@gmail.com>
2020-10-08 13:15:25 +0100

    labsis74h595

---
+++ Trabalho/Proj_atmega88_3/Atmega88/main.c
@@ -0,0 +1,18 @@
+/*
+ * Atmega88.c
+ *
+ * Created: 08/10/2020 13:13:01
+ * Author : sergi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
