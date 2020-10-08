ee4937f161b14de442de7013e1e127b0476615df
sergio1020881 <sergio.salazar.santos@gmail.com>
2020-10-08 22:33:58 +0100

    shift_data

--- Trabalho/Proj_atmega88_3/Atmega88/main.c
+++ Trabalho/Proj_atmega88_3/Atmega88/main.c
@@ -17,19 +17,23 @@ Author: Sergio Santos
 /***Constant and Macros***/
 #define TRUE 1
 #define ZERO 0
+#define Datapin 4
+#define Clkpin 5
+#define Outpin 7
 /***Global Variable***/
 /***Procedure & Function Def***/
 void PORTINIT();
-void shift_bit(uint8_t bool);
-void shift_byte(uint8_t byte);
+void shift_bit(uint8_t bool, uint8_t datapin, uint8_t clkpin);
+void shift_byte(uint8_t byte, uint8_t datapin, uint8_t clkpin, uint8_t outpin);
+void shift_data(uint8_t byte, uint8_t datapin, uint8_t clkpin, uint8_t outpin);
 /***MAINMAIN***/
 int main(void)
 {
 	//uint8_t i;
 	PORTINIT();
     /* Replace with your application code */
-	PORTD &= ~(1<<7); //output clock
-	PORTD &= ~(1<<5); //shift CLOCK input disable
+	PORTD &= ~(1<<Outpin); //Output clock
+	PORTD &= ~(1<<Clkpin); //Shift CLOCK input disable
     while (TRUE)
     {
 		
@@ -39,37 +43,46 @@ int main(void)
 		//for(i=0;i<8;i++)
 			//shift_bit(0);
 			
-		shift_byte(85);
-		shift_byte(170);
-		shift_byte(255);
-		shift_byte(0);
+		shift_data(85, 4, 5, 7);
+		shift_data(170, 4, 5, 7);
+		shift_data(255, 4, 5, 7);
+		shift_data(0, 4, 5, 7);
 			
     }
 }
 void PORTINIT()
 {
-	DDRD = (1<<4); //Serial Data
-	DDRD |= (1<<5); //Serial Clock
-	DDRD |= (1<<7); //Serial Store
-	PORTD = ~(1<<4); //Serial Data
-	PORTD &= ~(1<<5); //Serial Clock
-	PORTD &= ~(1<<7); //Serial Store
+	DDRD = (1<<Datapin); //Serial Data
+	DDRD |= (1<<Clkpin); //Serial Clock
+	DDRD |= (1<<Outpin); //Serial Store
+	PORTD = ~(1<<Datapin); //Serial Data
+	PORTD &= ~(1<<Clkpin); //Serial Clock
+	PORTD &= ~(1<<Outpin); //Serial Store
 }
-void shift_bit(uint8_t bool)
+void shift_bit(uint8_t bool, uint8_t datapin, uint8_t clkpin)
 {
 	_delay_ms(100);
 	if (bool)
-		PORTD |= (1<<4); //Data bit HIGH
+		PORTD |= (1<<datapin); //Data bit HIGH
 	else
-		PORTD &= ~(1<<4); //Data bit LOW
-	PORTD |= (1<<5); // shift bit
-	PORTD &= ~(1<<5); //shift disable
+		PORTD &= ~(1<<datapin); //Data bit LOW
+	PORTD |= (1<<clkpin); // Shift bit
+	PORTD &= ~(1<<clkpin); //Shift disable
 }
-void shift_byte(uint8_t byte)
+void shift_byte(uint8_t byte, uint8_t datapin, uint8_t clkpin, uint8_t outpin)
 {
 	uint8_t i;
 	for(i=0;i<8;i++)
-		shift_bit(byte & (1<<i));
-	PORTD |= (1<<7); //output enable
-	PORTD &= ~(1<<7); //output disable
+		shift_bit(byte & (1<<i), datapin, clkpin);
+	PORTD |= (1<<outpin); //Output enable
+	PORTD &= ~(1<<outpin); //Output disable
 }
+void shift_data(uint8_t byte, uint8_t datapin, uint8_t clkpin, uint8_t outpin)
+{
+	uint8_t i;
+	for(i=0;i<8;i++)
+		shift_bit(byte & (1<<i), datapin, clkpin);
+	PORTD |= (1<<outpin); //Output enable
+	PORTD &= ~(1<<outpin); //Output disable
+}
+/***EOF***/
