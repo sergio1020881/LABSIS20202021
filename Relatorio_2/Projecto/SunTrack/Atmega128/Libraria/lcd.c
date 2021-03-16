a7584d518c29d4649fd543dcd076c32e28bb751b
sergio1020881 <sergio.salazar.santos@gmail.com>
2021-03-16 02:24:39 +0000

    review_see_if_all_still_OK

--- Relatorio_2/Projecto/SunTrack/Atmega128/Libraria/lcd.c
+++ Relatorio_2/Projecto/SunTrack/Atmega128/Libraria/lcd.c
@@ -19,15 +19,14 @@ Comment:
 #include "lcd.h"
 /***Constant & Macro***/
 #ifndef GLOBAL_INTERRUPT_ENABLE
-	#define GLOBAL_INTERRUPT SREG
 	#define GLOBAL_INTERRUPT_ENABLE 7
 #endif
 //CMD RS
 #define INST 0
 #define DATA 1
 //ticks depends on CPU frequency this case 16Mhz
-#define LCD_N_TICKS 1
-#define LCD_BF_TICKS 20
+#define LCD_N_TICKS 0
+#define LCD_BF_TICKS 10
 /***Global File Variable***/
 volatile uint8_t *lcd0_DDR;
 volatile uint8_t *lcd0_PIN;
